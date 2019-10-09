aebd17797b1ebd64bbe9106b0bfca261ef55b79e
Fred Bednarski <cybrasty@gmail.com>
2019-10-08 23:33:12 -0400

    Basic State Machine working

--- js/game.js
+++ js/game.js
@@ -25,22 +25,22 @@ function draw_sprite(sprite,x,y) {
 }
 
 function draw_game() {
-  ctx.clearRect(0,0,canvas.width,canvas.height); //clear screen
-
-  //draw map
-  for(let i=0;i<num_tiles;i++){
-      for(let j=0;j<num_tiles;j++){
-          get_tile(i,j).draw();
-      }
+  if (game_state == "dungeon" || game_state == "dead"){
+    //clear screen
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    //draw map
+    for(let i=0;i<num_tiles;i++){
+        for(let j=0;j<num_tiles;j++){
+            get_tile(i,j).draw();
+        }
+    }
+    //draw monsters
+    for (let i=0; i<monsters.length;i++){
+      monsters[i].draw();
+    }
+    //draw player
+    player.draw();
   }
-
-  //draw monsters
-  for (let i=0; i<monsters.length;i++){
-    monsters[i].draw();
-  }
-
-  //draw player
-  player.draw();
 }
 
 function tick() {
@@ -51,4 +51,25 @@ function tick() {
       monsters.splice(k,1);
     }
   }
+  if (player.dead) {
+    game_state = "dead";
+  }
+}
+
+function show_title() {
+  ctx.fillStyle = 'rgba(0,0,0,.75)';
+  ctx.fillRect(0,0,canvas.width, canvas.height);
+  game_state = "title";
+}
+
+function start_game() {
+  level = 1;
+  start_level(starting_hp);
+  game_state = "dungeon";
+}
+
+function start_level(_hp) {
+  generate_level();
+  player = new Player(get_random_passable_tile());
+  player.hp = _hp;
 }
